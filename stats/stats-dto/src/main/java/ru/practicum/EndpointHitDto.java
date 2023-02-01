ce5a7be91ba44bb6a4c0d9075dfb67b8d9e5d78b
Eugene Krivinsky <Playa313@yandex.ru>
2023-02-01 22:34:12 +0300

    in progress: Переделываю архитектуру.

--- stats/stats-dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -5,21 +5,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 
-//import javax.validation.constraints.NotBlank;
 @Builder
 @Getter
 @Setter
 public class EndpointHitDto {
 
-//    @NotBlank
+
     private String app;
 
-//    @NotBlank
+
     private String uri;
 
-//    @NotBlank
+
     private String ip;
 
     private String timestamp;
